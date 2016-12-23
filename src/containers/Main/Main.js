ebb68366d1f0301432b6b0b2f19b255ba5b18fbd
ttop5 <ttop5@qq.com>
2016-12-23 11:17:11 +0800

    feat: fullPage效果

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -3,6 +3,7 @@ import style from './style.css';
 import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { SectionsContainer, Section } from 'react-fullpage';
 
 import * as MainActions from './actions';
 
@@ -36,10 +37,26 @@ class Main extends Component {
   }
 
   render() {
+    const options = {
+      sectionClassName: 'section',
+      anchors: ['sectionOne', 'sectionTwo', 'sectionThree'],
+      scrollBar: true,
+      verticalAlign: false,
+      arrowNavigation: true,
+      sectionPaddingTop: '0px',
+      sectionPaddingBottom: '0px',
+    };
+
     return (
       <div className={style.main}>
         <div className={style.content} >
-          <h1>statistical-charts</h1>
+          <SectionsContainer className="container" {...options}>
+            <Section className={style.section1} color="#007760">Page 1</Section>
+            <Section color="#00bcd4">Page 2</Section>
+            <Section color="#eeeeee">Page 3</Section>
+            <Section color="#eeeeee">Page 4</Section>
+            <Section color="#eeeeee">Page 5</Section>
+          </SectionsContainer>
         </div>
       </div>
     );
