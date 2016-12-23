3ca3357057ffc956cf14955325a61ea8341dc2ff
ttop5 <ttop5@qq.com>
2016-12-23 15:58:34 +0800

    style(FirstPage): 第一页基本样式

--- src/containers/Main/components/FirstPage/FirstPage.js
+++ src/containers/Main/components/FirstPage/FirstPage.js
@@ -1,8 +1,11 @@
-
 import style from './style.css';
 
 import classnames from 'classnames';
 import React, { Component, PropTypes } from 'react';
+import Button from 'antd/lib/button';
+import Icon from 'antd/lib/icon';
+import TweenOne from 'rc-tween-one';
+import QueueAnim from 'rc-queue-anim';
 
 
 class FirstPage extends Component {
@@ -28,7 +31,38 @@ class FirstPage extends Component {
         style={this.props.style}
         className={classnames(...className.split(), style.firstpage)}
       >
-        FirstPage
+        <div className={style.content}>
+          <QueueAnim
+            type={['bottom', 'top']}
+            delay={200}
+            className={`${this.props.className}-wrapper`}
+            key="text"
+          >
+            <span
+              className="title"
+              key="title"
+            >
+              <h1>STATISTICAL CHART</h1>
+            </span>
+            <p
+              key="content"
+            >
+              带您直观了解关于 SDUT OJ 背后的大数据
+            </p>
+            <Button type="ghost" key="button" className={style.button}>
+              <a href="http://sdutacm.org/">Go to Online Judge</a>
+            </Button>
+          </QueueAnim>
+        </div>
+        <div className={style.arraw}>
+          <TweenOne
+            animation={{ y: '-=20', yoyo: true, repeat: -1, duration: 1000 }}
+            className={`${this.props.className}-icon`}
+            key="icon"
+          >
+            <Icon type="down" />
+          </TweenOne>
+        </div>
       </div>
     );
   }
