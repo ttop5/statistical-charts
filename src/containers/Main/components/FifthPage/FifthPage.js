f730ef8e38c787586027bc9cd38606dc2fe14933
ttop5 <ttop5@qq.com>
2016-12-26 17:44:02 +0800

    feat(FifthPage): 第五页基本样式和图

--- src/containers/Main/components/FifthPage/FifthPage.js
+++ src/containers/Main/components/FifthPage/FifthPage.js
@@ -1,14 +1,22 @@
-
 import style from './style.css';
 
 import classnames from 'classnames';
 import React, { Component, PropTypes } from 'react';
+import { Tabs, Icon } from 'antd';
+import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
+import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
 
+import PieChart from './components/PieChart';
+
+
+const TabPane = Tabs.TabPane;
 
 class FifthPage extends Component {
   static propTypes = {
     style: PropTypes.string,
     className: PropTypes.string,
+    id: PropTypes.string,
   };
 
   static defaultProps = {
@@ -22,13 +30,79 @@ class FifthPage extends Component {
 
   render() {
     const { className } = this.props;
+    const [collegeIdName, schoolIdName, majorIdName] = ['collegePieChart', 'schoolPieChart', 'majorPieChart'];
+    const collegePieChartData = [
+      { value: 2335, name: '山东理工' },
+      { value: 310, name: '山东科技' },
+      { value: 234, name: '中国石油(华东)' },
+      { value: 135, name: '青岛理工' },
+      { value: 548, name: '青岛大学' },
+    ];
+    const schoolPieChartData = [
+      { value: 335, name: '计算机学院' },
+      { value: 310, name: '电气学院' },
+      { value: 234, name: '车辆工程学院' },
+      { value: 135, name: '理学院' },
+      { value: 548, name: '材料学院' },
+    ];
+    const majorPieChartData = [
+      { value: 2935, name: '计算机科学与技术' },
+      { value: 310, name: '软件工程' },
+      { value: 234, name: '通信工程' },
+      { value: 135, name: '高分子材料' },
+      { value: 48, name: '生物化学' },
+    ];
 
+    /* eslint-disable */
     return (
       <div
         style={this.props.style}
         className={classnames(...className.split(), style.fifthpage)}
       >
-        FifthPage
+        <OverPack
+          className={`content-template ${this.props.className}`}
+          hideProps={{ img: { reverse: true } }}
+          location={this.props.id}
+        >
+          <TweenOne
+            key="img"
+            animation={{ x: '-=30', opacity: 0, type: 'from' }}
+            className={`${this.props.className}-img`}
+          >
+            <div className={style.countTitle}>
+              <span>用户分布</span>
+            </div>
+          </TweenOne>
+
+          <QueueAnim
+            className={`${this.props.className}-text`}
+            key="text"
+            leaveReverse
+            ease={['easeOutCubic', 'easeInCubic']}
+          >
+            <Tabs defaultActiveKey="1" key="tabs" className={style.tabs}>
+              <TabPane tab={<span><Icon type="pie-chart" />专业分布</span>} key="1">
+                <PieChart
+                  idName={majorIdName}
+                  chartData={majorPieChartData}
+                />
+              </TabPane>
+              <TabPane tab={<span><Icon type="pie-chart" />学院分布</span>} key="2">
+                <PieChart
+                  idName={schoolIdName}
+                  chartData={schoolPieChartData}
+                />
+              </TabPane>
+              <TabPane tab={<span><Icon type="pie-chart" />学校分布</span>} key="3">
+                <PieChart
+                  idName={collegeIdName}
+                  chartData={collegePieChartData}
+                />
+              </TabPane>
+            </Tabs>
+          </QueueAnim>
+        </OverPack>
+
       </div>
     );
   }
---
+++ src/containers/Main/components/FifthPage/components/PieChart/PieChart.js
@@ -0,0 +1,69 @@
+import style from './style.css';
+
+import classnames from 'classnames';
+import React, { Component, PropTypes } from 'react';
+import echarts from 'echarts';
+
+
+class PieChart extends Component {
+  static propTypes = {
+    style: PropTypes.string,
+    className: PropTypes.string,
+    idName: PropTypes.string.isRequired,
+    chartData: PropTypes.object.isRequired,
+  };
+
+  static defaultProps = {
+    className: '',
+  };
+
+  constructor(props, context) {
+    super(props, context);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { idName, chartData } = this.props;
+    const myChart = echarts.init(document.getElementById(idName));
+    const option = {
+      color: ['#2196f3', '#1f939e', '#00bcd4', '#009688', '#37bdab'],
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)',
+      },
+      series: [
+        {
+          name: '用户学校分布',
+          type: 'pie',
+          radius: '85%',
+          center: ['50%', '55%'],
+          data: chartData,
+          itemStyle: {
+            emphasis: {
+              shadowBlur: 10,
+              shadowOffsetX: 0,
+              shadowColor: 'rgba(0, 0, 0, 0.5)',
+            },
+          },
+        },
+      ],
+    };
+    myChart.setOption(option);
+  }
+
+  /* eslint-disable */
+  render() {
+    const { className, idName } = this.props;
+
+    return (
+      <div
+        style={this.props.style}
+        className={classnames(...className.split(), style.piechart)}
+      >
+        <div key="chart" id={idName} className={style.pieChart}></div>
+      </div>
+    );
+  }
+}
+
+export default PieChart;
